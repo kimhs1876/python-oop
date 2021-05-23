9cc946c51ec091d23ca01a8cf000a17cac06f4a5
kimhs1876 <kimhs1876@naver.com>
2021-05-23 10:51:35 +0900

    practice_ready

--- list/stock.py
+++ list/stock.py
@@ -1,10 +1,11 @@
 class Stock(object):
+
     def __init__(self, name, code):
         self.name = name
         self.code = code
 
     def to_string(self):
-        return f' 종목이름 {self.name}, 종목코드 {self.code}'
+        return f'종목명 {self.name} 종목코드 {self.code}'
 
     @staticmethod
     def del_element(ls, code):
@@ -14,11 +15,10 @@ class Stock(object):
 
     @staticmethod
     def main():
-        ls =[]
+        ls = []
         while 1:
-            menu = input('0.Exit, 1.Create, 2.Read 3.Update 4.Delete')
+            menu = input('0.Exit 1.Create 2.Read 3.Update 4.Delete')
             if menu == '0':
-                print('프로그램을 종료합니다')
                 break
             elif menu == '1':
                 ls.append(Stock(input('name'), input('code')))
@@ -26,14 +26,17 @@ class Stock(object):
                 for i in ls:
                     print(i.to_string())
             elif menu == '3':
-                code = input("종목코드")
+                code = input('종목코드')
                 stock = Stock(input('수정할 이름'), code)
                 stock.del_element(ls, code)
-                ls.append(Stock)
+                ls.append(stock)
+
             elif menu == '4':
                 stock = Stock(None, input('종목코드'))
                 stock.del_element(ls, stock.code)
             else:
-                print('숫자를 다시 입력하세요')
+                print('Wrong Number')
                 continue
+
+
 Stock.main()
